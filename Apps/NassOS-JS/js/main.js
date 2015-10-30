b1a210bcded027da895117b4be760fd4d09232d8
Nasso <nassomails@gmail.com>
2015-10-30 23:38:18 +0100

    Fixed background bug

--- Apps/NassOS-JS/js/main.js
+++ Apps/NassOS-JS/js/main.js
@@ -42,15 +42,13 @@ function loop(){
 }
 
 function start(){
+	initCanvas();
+	
 	var back = new Image();
 	back.onload = function(){
 		nindow.setBackground(this);
 	};
 	back.src = "res/back.jpg";
-	
-	initCanvas();
-	
-	nindow.background = "#007BA2"
 	
 	nindow.addWindow("Hello world", {x: null, y: null}, {width: 400, height: 400});
 	nindow.addWindow("Foo", {x: 50, y: 50}, {width: 800, height: 600});
