4737ec0bd906dbcae40a3a74f971d1e0946a86b1
nasso <nassomails@gmail.com>
2018-04-09 18:25:40 +0200

    Added basic dynamic content system

--- script/index.js
+++ script/index.js
@@ -1,8 +1,32 @@
-const el_letter_paths = document.getElementsByClassName("letter-path");
+var el_main = document.getElementsByTagName("main")[0];
+var el_content = document.getElementById("content");
+var el_container = document.getElementById("container");
 
-// Reset dash array (animation)
-setTimeout(function() {
-	for(let i = 0; i < el_letter_paths.length; i++) {
-		el_letter_paths[i].classList.remove("animating");
+function loadContent(hash) {
+	if(hash === "") {
+		el_content.classList.toggle("active", false);
+		el_main.classList.remove("hidden");
+		el_main.classList.remove("animating");
+		el_main.classList.add("animating");
+	} else {
+		el_main.classList.add("hidden");
+
+		el_content.classList.toggle("active", false);
+
+		setTimeout(function() {
+			var xhr = new XMLHttpRequest();
+			xhr.onload = function() {
+				el_container.innerHTML = xhr.response;
+				el_content.classList.toggle("active", true);
+			};
+			xhr.open("GET", "./content/about.html");
+			xhr.send();
+		}, 500);
 	}
-}, 3000);
+}
+
+loadContent(window.location.hash);
+
+window.addEventListener("hashchange", function() {
+	loadContent(window.location.hash);
+});
